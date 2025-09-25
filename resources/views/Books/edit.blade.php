484e3fee4cb628a3d392e2eae6d4963420c150f8
zakria hassan <xsakriye212@gmail.com>
2025-09-25 10:45:41 +0300

    update the code

--- resources/views/Books/edit.blade.php
+++ resources/views/Books/edit.blade.php
@@ -1,4 +1,4 @@
-<!-- <x-layout>
+<x-layout>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -30,4 +30,4 @@
             </div>
         </div>
     </div>
-</x-layout> -->
+</x-layout>
